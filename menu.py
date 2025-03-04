75947654c3fb28c0d6d6268e29c933811e68abfe
nmiya <nmiyaguc@calpoly.edu>
2025-03-04 10:37:59 -0800

    created menu creation program using file sorting

---
+++ menu.py
@@ -0,0 +1,14 @@
+# this is used to change the menu that the program will process.
+# the .txt file that is entered should have three lines, each with corresponding menu items, prices, and prep times.
+# 3 lines in total
+
+establishment = "ristorante_italia.txt"
+
+#using the entered .txt file, initializes the three core lists for the program.
+try:
+    with open(establishment) as file:
+        menu = file.readline().rstrip("\n").split("~")
+        prices = file.readline().rstrip("\n").split("~")
+        prep_time = file.readline().rstrip("\n").split("~")
+except FileNotFoundError as e:
+    print("Error: " + str(e))
