8b429743b20988e785a85d997d9908dcf283d9fb
nmiya <nmiyaguc@calpoly.edu>
2025-03-05 15:43:47 -0800

    example prices function added

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
+from os import write
+
 import input
 
 # this is used to change the menu that the program will process.
 # the .txt file that is entered should have three lines, each with corresponding menu items, prices, and prep times.
 # 3 lines in total
-
 establishment = "ristorante_italia.txt"
+
+# "order_tracker.txt" keeps track of the total orders to ensure that each new order has a unique order number
+order_nums = "order_tracker.txt"
 
 #using the entered .txt file, initializes the three core lists for the program.
 try:
@@ -14,7 +18,13 @@ try:
         prep_time = file.readline().rstrip("\n").split("~")
 except FileNotFoundError as e:
     print("Error: " + str(e))
-    print("hello")
+
+# uses the order_nums variable to initialize the total_orders variable.
+try:
+    with open(order_nums) as file:
+        total_orders = int(file.readline())
+except FileNotFoundError as e:
+    print("Error: " + str(e))
 
 # Prices function here
 order_list = [0,2,3]
@@ -22,12 +32,23 @@ order_list = [0,2,3]
 x = 0
 for idx in order_list:
     x = x + float(prices[idx])
-print(x)
-
 
 # Order number function here
+# this function outputs a unique string everytime it is called, which acts as an order number
+# no input is required
+def create_order_num() -> str:
+    global total_orders
+    with open(order_nums, "w") as file:
+        file.write(str(total_orders + 1))
 
+    if total_orders < 10:
+        order_num = "00" + str(total_orders)
+    elif total_orders < 100:
+        order_num = "0" + str(total_orders)
+    else:
+        order_num = str(total_orders)
 
+    return order_num
 
 # Time function here
 
--- input.py
+++ input.py
@@ -3,19 +3,16 @@ import functions
 if __name__ == '__main__':
     user_input = ""
     order_index_list = []
-    while user_input != "close":
-        while user_input != "done":
-            user_input = input("Enter item number. Type 'done' when you are finished with the order, or 'close' to end the program: ")
-            try:
+    while user_input != "done":
+        user_input = input("Enter item number. Type 'done' when you are finished with the order, or 'close' to end the program: ")
+        try:
+            order_index_list.append(int(user_input))
+        except ValueError:
+            if user_input == "done":
+                print(order_index_list)
+                # call all functions in functions.py
+                order_index_list = []
+                break
+            else:
+                user_input = input("Error, invalid input. enter an item number: ")
                 order_index_list.append(int(user_input))
-            except ValueError:
-                if user_input == "done":
-                    print(order_index_list)
-                    # call all functions in functions.py
-                    order_index_list = []
-                    user_input = input("Enter item number. Type 'done' when you are finished with the order, or 'close' to end the program: ")
-                elif user_input == "close":
-                    print("Stopping execution...")
-                    exit()
-                else:
-                    user_input = input("Error, invalid input. enter an item number: ")
